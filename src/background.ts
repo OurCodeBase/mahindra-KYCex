188df1348a79d0856c7b612fd328b400a6806b34
hαrsh vαirαgi <harshb7839@gmail.com>
2025-10-16 17:37:39 +0000

    added logging and host permission pattern manipulated.

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,9 @@
 const onMessage = (message: any) => {
   if (message.type == 'FIND_TOKENS') {
+    console.log("Listened a web request for tokens...");
     const data: Record<string, string> = {}
     const onBeforeSendHeaders = (details: chrome.webRequest.OnBeforeSendHeadersDetails) => {
+      console.log("Found a web request at url:", details.url);
       if (!details.requestHeaders) return details;
       for (const header of details.requestHeaders) {
         if (['userid', 'accesstoken', 'authorization'].includes(header.name)) {
@@ -10,14 +12,18 @@ const onMessage = (message: any) => {
       }
       chrome.webRequest.onBeforeSendHeaders.removeListener(onBeforeSendHeaders);
       if (Object.keys(data).length == 3) {
+        console.log("Saved tokens in session storage:", data);
         chrome.storage.session.set({ "robin-authtokens": JSON.stringify(data) })
       }
       return details;
     }
     chrome.webRequest.onBeforeSendHeaders.addListener(
-      onBeforeSendHeaders,
-      { urls: ["http://localhost:5000/api/*", "https://api.mahindradealerrise.com/otf/vehicleinvoice/*"] },
-      ["requestHeaders"]
+      onBeforeSendHeaders, {
+        urls: [
+          "http://localhost:5000/api/*",
+          "https://api.mahindradealerrise.com/otf/vehicleinvoice/*"
+        ]
+      }, ["requestHeaders"]
     );
   }
   return true;
--- src/hooks/token.tsx
+++ src/hooks/token.tsx
@@ -14,11 +14,13 @@ type TokenHook = {
 export default function App(): TokenHook {
   const [token, setToken] = useState<Token | null>(null);
   useEffect(() => {
+    console.log("Checking token availablity...")
     chrome.storage.session.get("robin-authtokens").then((response) => {
       if (response && response["robin-authtokens"]) {
         const authtokens = JSON.parse(response["robin-authtokens"])
         setToken(authtokens)
       } else {
+        console.log("Calling web request for tokens...");
         chrome.runtime.sendMessage({ type: 'FIND_TOKENS' })
       }
     })
