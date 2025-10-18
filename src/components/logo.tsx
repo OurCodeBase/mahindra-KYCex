e1de83d25938aa087b7c7a4bc41d7b5b5cb78a0a
hαrsh vαirαgi <harshb7839@gmail.com>
2025-10-18 19:57:41 +0000

    some bugs has been fixed.
    - ui has been fixed.
    - now 2nd form fill will work.
    - hardcode converted to iteration.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex justify-center py-4 flex-col items-center">
-      <img src="/logo.png" className="w-30 brightness-200"/>
+      <img src="/logo.png" className="w-30"/>
     </div>
   )
 }
